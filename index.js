456bedfb2ed9806ecb0c7d7e29449c192eecbf5d
Aryan <aryan.pathania2003@gmail.com>
2022-10-05 07:24:55 +0530

    📝 Added `--help` flag

--- index.js
+++ index.js
@@ -3,7 +3,19 @@
 import meow from 'meow';
 import parseGH from './parse.js';
 
-const cli = meow(``, {
+const helpText = `
+Usage:
+$ github-to-json <owner> <repo>
+
+Options:
+-d, --depth  <depth> Depth upto which JSON is to be genrated
+
+-t, --token  <token> GitHub PAT
+
+-o, --output <file>  Output file name, by default the output is printed directly to the cli
+`;
+
+const cli = meow(helpText, {
   importMeta: import.meta,
   flags: {
     depth: {
